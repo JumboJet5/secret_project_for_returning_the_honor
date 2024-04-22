42c69de741742f35c6d8b2111889264d6365a985
ivan_pogybko <pogybkoivan@gmail.com>
2024-04-22 22:59:26 +0300

    add working runner

--- js/app.ts
+++ js/app.ts
@@ -1,1 +1,27 @@
+import { fromEvent } from "rxjs";
+
 console.log('Hello World!')
+
+let x: number | null = null
+
+const container = document.getElementById('container')
+const pointer = document.getElementById('pointer')
+
+const $mousedown = fromEvent(container, 'mousedown')
+const $mousemove = fromEvent(container, 'mousemove')
+const $mouseup = fromEvent(container, 'mouseup')
+
+$mousedown.subscribe((event: MouseEvent) => {
+  const x = event.pageX - container.offsetLeft
+  pointer.style.setProperty('--x', `${x}px`)
+})
+
+$mousemove.subscribe((event: MouseEvent) => {
+  const x = event.pageX - container.offsetLeft
+  pointer.style.setProperty('--x', `${x}px`)
+})
+
+$mouseup.subscribe(() => {
+  x = null
+  console.log(x)
+})
